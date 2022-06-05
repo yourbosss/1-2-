ba351490643259467d0de57b3c9824114b3e8c8d
DESKTOP-DUSKVKU\Ксюша <kseniia.bosss@gmail.com>
2022-06-05 16:13:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/**********************************************
+ * Автор: Копытова К.К.                       *
+ * Дата: 05.06.2022                           *
+ * Название: Базовые понятия языка С# (2 лаба)*
+**********************************************/
+
+using System;
+
+namespace namespaceOne
+{
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Введите x:");
+			var Number = Console.ReadLine();
+
+			int NumberInt;
+
+			if (!int.TryParse(Number, out NumberInt) || NumberInt < 100) // преодразовывает число 
+			{
+
+				Console.WriteLine("\n" + "Неправильное число");
+
+				return;
+			}
+
+			Console.Clear();
+
+			Console.WriteLine("x: " + Number);
+
+			Number = Number.Remove(1, 1) + Number.Substring(1, Number.Length - (Number.Length - 1)); //удаление + извлечение строки
+
+			Console.WriteLine("n: " + Number);
+
+			Console.ReadKey();
+		}
+	}
+}
